ddaa00b32c560e801ee1e1ca0b55482a855d939c
Nikita <worldmixgames@gmail.com>
2020-01-12 14:03:30 +0200

    fix floor

--- editor/srcs/control.c
+++ editor/srcs/control.c
@@ -6,7 +6,7 @@
 /*   By: nlavrine <nlavrine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 14:20:18 by nlavrine          #+#    #+#             */
-/*   Updated: 2019/11/29 17:00:32 by nlavrine         ###   ########.fr       */
+/*   Updated: 2020/01/12 14:02:00 by nlavrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ void	right_click_event(t_editor *editor, SDL_Event event)
 	void		*param;
 
 	SDL_GetMouseState(&mouse_position.x, &mouse_position.y);
-		if ((param = check_line(editor, mouse_position)))
-			choice_win(editor, event, 1, param);
-		else if ((param = check_rooms(editor, mouse_position, 1)))
-			choice_win(editor, event, 2, param);
-		else if ((param = check_sprite(editor->rooms, mouse_position, editor->zoom)))
-			choice_win(editor, event, 3, param);
+	if ((param = check_line(editor, mouse_position)))
+		choice_win(editor, event, 1, param);
+	else if ((param = check_sprite(editor->rooms, mouse_position, editor->zoom)))
+		choice_win(editor, event, 3, param);
+	else if ((param = check_rooms(editor, mouse_position, 1)))
+		choice_win(editor, event, 2, param);
 }
 
 void	mouse_events(t_editor *editor, SDL_Event event)
 {
 	if (event.type == SDL_MOUSEWHEEL)
 		mouse_zoom(editor, event);
-	if (event.type == SDL_MOUSEBUTTONDOWN)
+	if (event.button.clicks == 1 && event.button.button == SDL_BUTTON_LEFT)
 		mouse_button_down(editor);
 	if (event.button.clicks == 1 && event.button.button == SDL_BUTTON_RIGHT)
 		right_click_event(editor, event);
